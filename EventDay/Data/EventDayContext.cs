1d0963d3756a3a8fe11a2302edefa44efa8bc2a7
jansan <jansan-26@hotmail.com>
2020-12-10 10:11:43 +0100

    DbContext created

---
+++ EventDay/Data/EventDayContext.cs
@@ -0,0 +1,16 @@
+﻿using EventDay.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventDay.Data
+{
+    public class EventDayContext : DbContext
+    {
+        public DbSet<EventDay.Models.Entities.EventDay> EventDays { get; set; }
+       
+        public EventDayContext(DbContextOptions<EventDayContext> options) : base(options){ }
+    }
+}
