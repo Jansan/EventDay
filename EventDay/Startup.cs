2bf5e8560f5dc141186c1e4cde70d4eb0ecd3233
jansan <jansan-26@hotmail.com>
2020-12-10 16:21:27 +0100

    EventRepository and AutoMapper

--- EventDay/Startup.cs
+++ EventDay/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using EventDay.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -29,8 +30,10 @@ namespace EventDay
         {
             services.AddControllers();
 
-            services.AddDbContext<EventDayContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString
-                ("EventContext")));
+            services.AddDbContext<EventDayContext>(opt => opt.UseSqlServer
+            (Configuration.GetConnectionString("EventContext")));
+
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
