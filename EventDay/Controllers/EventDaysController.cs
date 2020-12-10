9b2eb091e31ba5dfb518db70d856e936b77ba0a8
jansan <jansan-26@hotmail.com>
2020-12-10 16:42:01 +0100

    AutoMapper

--- EventDay/Controllers/EventDaysController.cs
+++ EventDay/Controllers/EventDaysController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using EventDay.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,10 +14,12 @@ namespace EventDay.Controllers
     public class EventDaysController : ControllerBase
     {
         private EventRepository repo;
+        private readonly IMapper mapper;
 
-        public EventDaysController(EventDayContext context)
+        public EventDaysController(EventDayContext context, IMapper mapper)
         {
             repo = new EventRepository(context);
+            this.mapper = mapper;
             
         }
 
